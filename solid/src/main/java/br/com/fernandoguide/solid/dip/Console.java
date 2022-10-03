985ae6ca934aa3c6934c849ab03ab030cd02f489
fernandoguide <fernando.abreu.ext@dasa.com.br>
2022-10-03 12:54:58 -0300

    att

--- solid/src/main/java/br/com/fernandoguide/solid/dip/Console.java
+++
@@ -1,5 +0,0 @@
-package br.com.fernandoguide.solid.dip;
-
-public interface Console {
-        String show(String s1,String s2);
-}
---
+++ src/main/java/br/com/fernandoguide/solid/dip/Console.java
@@ -0,0 +1,6 @@
+package br.com.fernandoguide.solid.dip;
+
+@FunctionalInterface
+public interface Console {
+        String show(String s1,String s2);
+}
